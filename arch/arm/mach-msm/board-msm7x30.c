d1a93852a9966c7c2f4b6f5443708d022b544e15
Alhad Purnapatre <alhadp@codeaurora.org>
2012-02-28 13:24:57 -0800

    msm_fb: display: MSM V4l2 video overlay driver
    Provides a V4L2 device that uses the MDP overlay pipes (on MDP4),
    or the PPP interface (MDP3) to overlay frames on top of display
    framebuffer.
    
    Signed-off-by: Alhad Purnapatre <alhadp@codeaurora.org>
    Change-Id: Iab69d0a5acfe993d13cb7a585e292b9a87eb90ee
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-msm7x27a.c
    	arch/arm/mach-msm/board-msm7x30.c
    	drivers/media/video/msm/Kconfig
    	drivers/video/msm/mdp_ppp.c

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -97,6 +97,245 @@ static void __init msm7x30_init(void)
 	msm_device_hsusb_host.dev.parent = &msm_device_otg.dev;
 
 	platform_add_devices(devices, ARRAY_SIZE(devices));
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_USB_EHCI_MSM_72K
+	msm_add_host(0, &msm_usb_host_pdata);
+#endif
+	msm7x30_init_mmc();
+	msm7x30_init_nand();
+	msm_qsd_spi_init();
+
+#ifdef CONFIG_SPI_QSD
+	if (machine_is_msm7x30_fluid())
+		spi_register_board_info(lcdc_sharp_spi_board_info,
+			ARRAY_SIZE(lcdc_sharp_spi_board_info));
+	else
+		spi_register_board_info(lcdc_toshiba_spi_board_info,
+			ARRAY_SIZE(lcdc_toshiba_spi_board_info));
+#endif
+
+	atv_dac_power_init();
+	sensors_ldo_init();
+	hdmi_init_regs();
+	msm_fb_add_devices();
+	msm_pm_set_platform_data(msm_pm_data, ARRAY_SIZE(msm_pm_data));
+	BUG_ON(msm_pm_boot_init(&msm_pm_boot_pdata));
+	msm_device_i2c_init();
+	msm_device_i2c_2_init();
+	qup_device_i2c_init();
+	msm7x30_init_marimba();
+#ifdef CONFIG_MSM7KV2_AUDIO
+	snddev_poweramp_gpio_init();
+	snddev_hsed_voltage_init();
+	aux_pcm_gpio_init();
+#endif
+
+	i2c_register_board_info(0, msm_i2c_board_info,
+			ARRAY_SIZE(msm_i2c_board_info));
+
+	if (!machine_is_msm8x55_svlte_ffa() && !machine_is_msm7x30_fluid())
+		marimba_pdata.tsadc = &marimba_tsadc_pdata;
+
+	if (machine_is_msm7x30_fluid())
+		i2c_register_board_info(0, cy8info,
+					ARRAY_SIZE(cy8info));
+#ifdef CONFIG_BOSCH_BMA150
+	if (machine_is_msm7x30_fluid())
+		i2c_register_board_info(0, bma150_board_info,
+					ARRAY_SIZE(bma150_board_info));
+#endif
+
+	i2c_register_board_info(2, msm_marimba_board_info,
+			ARRAY_SIZE(msm_marimba_board_info));
+
+	i2c_register_board_info(2, msm_i2c_gsbi7_timpani_info,
+			ARRAY_SIZE(msm_i2c_gsbi7_timpani_info));
+
+	i2c_register_board_info(4 /* QUP ID */, msm_camera_boardinfo,
+				ARRAY_SIZE(msm_camera_boardinfo));
+
+	bt_power_init();
+#ifdef CONFIG_I2C_SSBI
+	msm_device_ssbi7.dev.platform_data = &msm_i2c_ssbi7_pdata;
+#endif
+	if (machine_is_msm7x30_fluid())
+		i2c_register_board_info(0, msm_isa1200_board_info,
+			ARRAY_SIZE(msm_isa1200_board_info));
+
+#if defined(CONFIG_TOUCHSCREEN_TSC2007) || \
+	defined(CONFIG_TOUCHSCREEN_TSC2007_MODULE)
+	if (machine_is_msm8x55_svlte_ffa())
+		i2c_register_board_info(2, tsc_i2c_board_info,
+				ARRAY_SIZE(tsc_i2c_board_info));
+#endif
+
+	if (machine_is_msm7x30_surf())
+		platform_device_register(&flip_switch_device);
+
+	pm8058_gpios_init();
+
+	if (machine_is_msm7x30_fluid()) {
+		/* Initialize platform data for fluid v2 hardware */
+		if (SOCINFO_VERSION_MAJOR(
+				socinfo_get_platform_version()) == 2) {
+			cy8ctma300_pdata.res_y = 920;
+			cy8ctma300_pdata.invert_y = 0;
+		}
+		i2c_register_board_info(0, cy8ctma300_board_info,
+			ARRAY_SIZE(cy8ctma300_board_info));
+	}
+
+	if (machine_is_msm8x55_svlte_surf() || machine_is_msm8x55_svlte_ffa()) {
+		rc = gpio_tlmm_config(usb_hub_gpio_cfg_value, GPIO_CFG_ENABLE);
+		if (rc)
+			pr_err("%s: gpio_tlmm_config(%#x)=%d\n",
+				__func__, usb_hub_gpio_cfg_value, rc);
+	}
+
+	boot_reason = *(unsigned int *)
+		(smem_get_entry(SMEM_POWER_ON_STATUS_INFO, &smem_size));
+	printk(KERN_NOTICE "Boot Reason = 0x%02x\n", boot_reason);
+}
+
+static unsigned pmem_sf_size = MSM_PMEM_SF_SIZE;
+static int __init pmem_sf_size_setup(char *p)
+{
+	pmem_sf_size = memparse(p, NULL);
+	return 0;
+}
+early_param("pmem_sf_size", pmem_sf_size_setup);
+
+static unsigned fb_size = MSM_FB_SIZE;
+static int __init fb_size_setup(char *p)
+{
+	fb_size = memparse(p, NULL);
+	return 0;
+}
+early_param("fb_size", fb_size_setup);
+
+static unsigned pmem_adsp_size = MSM_PMEM_ADSP_SIZE;
+static int __init pmem_adsp_size_setup(char *p)
+{
+	pmem_adsp_size = memparse(p, NULL);
+	return 0;
+}
+early_param("pmem_adsp_size", pmem_adsp_size_setup);
+
+static unsigned fluid_pmem_adsp_size = MSM_FLUID_PMEM_ADSP_SIZE;
+static int __init fluid_pmem_adsp_size_setup(char *p)
+{
+	fluid_pmem_adsp_size = memparse(p, NULL);
+	return 0;
+}
+early_param("fluid_pmem_adsp_size", fluid_pmem_adsp_size_setup);
+
+static unsigned pmem_audio_size = MSM_PMEM_AUDIO_SIZE;
+static int __init pmem_audio_size_setup(char *p)
+{
+	pmem_audio_size = memparse(p, NULL);
+	return 0;
+}
+early_param("pmem_audio_size", pmem_audio_size_setup);
+
+static unsigned pmem_kernel_ebi0_size = PMEM_KERNEL_EBI0_SIZE;
+static int __init pmem_kernel_ebi0_size_setup(char *p)
+{
+	pmem_kernel_ebi0_size = memparse(p, NULL);
+	return 0;
+}
+early_param("pmem_kernel_ebi0_size", pmem_kernel_ebi0_size_setup);
+
+static struct memtype_reserve msm7x30_reserve_table[] __initdata = {
+	[MEMTYPE_SMI] = {
+	},
+	[MEMTYPE_EBI0] = {
+		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
+	},
+	[MEMTYPE_EBI1] = {
+		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
+	},
+};
+
+static void __init size_pmem_devices(void)
+{
+#ifdef CONFIG_ANDROID_PMEM
+	unsigned long size;
+
+	if machine_is_msm7x30_fluid()
+		size = fluid_pmem_adsp_size;
+	else
+		size = pmem_adsp_size;
+	android_pmem_adsp_pdata.size = size;
+	android_pmem_audio_pdata.size = pmem_audio_size;
+	android_pmem_pdata.size = pmem_sf_size;
+#endif
+}
+
+static void __init reserve_memory_for(struct android_pmem_platform_data *p)
+{
+	msm7x30_reserve_table[p->memory_type].size += p->size;
+}
+
+static void __init reserve_pmem_memory(void)
+{
+#ifdef CONFIG_ANDROID_PMEM
+	reserve_memory_for(&android_pmem_adsp_pdata);
+	reserve_memory_for(&android_pmem_audio_pdata);
+	reserve_memory_for(&android_pmem_pdata);
+	msm7x30_reserve_table[MEMTYPE_EBI0].size += pmem_kernel_ebi0_size;
+#endif
+}
+
+static void __init msm7x30_calculate_reserve_sizes(void)
+{
+	size_pmem_devices();
+	reserve_pmem_memory();
+}
+
+static int msm7x30_paddr_to_memtype(unsigned int paddr)
+{
+	if (paddr < phys_add)
+		return MEMTYPE_EBI0;
+	if (paddr >= phys_add && paddr < 0x80000000)
+		return MEMTYPE_EBI1;
+	return MEMTYPE_NONE;
+}
+
+static struct reserve_info msm7x30_reserve_info __initdata = {
+	.memtype_reserve_table = msm7x30_reserve_table,
+	.calculate_reserve_sizes = msm7x30_calculate_reserve_sizes,
+	.paddr_to_memtype = msm7x30_paddr_to_memtype,
+};
+
+static void __init msm7x30_reserve(void)
+{
+	reserve_info = &msm7x30_reserve_info;
+	msm_reserve();
+}
+
+static void __init msm7x30_allocate_memory_regions(void)
+{
+	void *addr;
+	unsigned long size;
+
+	size = fb_size ? : MSM_FB_SIZE;
+	addr = alloc_bootmem_align(size, 0x1000);
+	msm_fb_resources[0].start = __pa(addr);
+	msm_fb_resources[0].end = msm_fb_resources[0].start + size - 1;
+	pr_info("allocating %lu bytes at %p (%lx physical) for fb\n",
+		size, addr, __pa(addr));
+
+#ifdef CONFIG_MSM_V4L2_VIDEO_OVERLAY_DEVICE
+	size = MSM_V4L2_VIDEO_OVERLAY_BUF_SIZE;
+	addr = alloc_bootmem_align(size, 0x1000);
+	msm_v4l2_video_overlay_resources[0].start = __pa(addr);
+	msm_v4l2_video_overlay_resources[0].end =
+		msm_v4l2_video_overlay_resources[0].start + size - 1;
+	pr_debug("allocating %lu bytes at %p (%lx physical) for v4l2\n",
+		size, addr, __pa(addr));
+#endif
+>>>>>>> cd58e61... msm_fb: display: MSM V4l2 video overlay driver
 }
 
 static void __init msm7x30_map_io(void)
